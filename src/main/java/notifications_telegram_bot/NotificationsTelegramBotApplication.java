78cd7d4b8a7d91d59d8a6de33ed8bff732a9621c
2desoo <marcus60.2001@gmail.com>
2024-06-21 14:24:42 +0300

    Initial commit

---
+++ src/main/java/notifications_telegram_bot/NotificationsTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package notifications_telegram_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationsTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationsTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/notifications_telegram_bot/NotificationsTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package notifications_telegram_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationsTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
